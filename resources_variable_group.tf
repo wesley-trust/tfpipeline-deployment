8ce6c35e161ded5046f8b1b79661dc0ba89995e5
wesley-trust <wesley-trust@users.noreply.github.com>
2022-08-04 17:29:52 +0100

    Add new VG

---
+++ resources_variable_group.tf
@@ -0,0 +1,55 @@
+resource "azuredevops_variable_group" "terraform-backend" {
+  project_id   = local.project_id
+  name         = "terraform-backend-01"
+  description  = "Managed by Terraform"
+  allow_access = true
+
+  variable {
+    name  = "backendAzureRmKey"
+    value = "terraform.tfstate"
+  }
+
+  variable {
+    name  = "backendAzureRmResourceGroupLocation"
+    value = "uksouth"
+  }
+
+  variable {
+    name  = "backendAzureRmResourceGroupName"
+    value = "TerraformServices-rg"
+  }
+
+  variable {
+    name  = "backendAzureRmStorageAccountName"
+    value = "terraformsvcsprodsa"
+  }
+
+  variable {
+    name  = "backendServiceArm"
+    value = "ARM-Alpha"
+  }
+}
+
+resource "azuredevops_variable_group" "terraform-global" {
+  project_id   = local.project_id
+  name         = "terraform-global-01"
+  description  = "Managed by Terraform"
+  allow_access = true
+
+  variable {
+    name  = "DelayMinutes"
+    value = "5"
+  }
+}
+
+resource "azuredevops_variable_group" "terraform-system" {
+  project_id   = local.project_id
+  name         = "terraform-system-01"
+  description  = "Managed by Terraform"
+  allow_access = true
+
+  variable {
+    name  = "terraformVersion"
+    value = "1.0.4"
+  }
+}
