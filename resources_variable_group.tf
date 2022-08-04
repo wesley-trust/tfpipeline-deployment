1c18e0c676c2f3ba457cc214cef17f191c878d5e
wesley-trust <wesley-trust@users.noreply.github.com>
2022-08-04 17:41:16 +0100

    Update variable group

--- resources_variable_group.tf
+++ resources_variable_group.tf
@@ -1,3 +1,35 @@
+resource "azuredevops_variable_group" "terraform-backend" {
+  project_id   = local.project_id
+  name         = "terraform-backend"
+  description  = "Managed by Terraform"
+  allow_access = true
+
+  variable {
+    name  = "backendAzureRmKey"
+    value = "terraform.tfstate"
+  }
+
+  variable {
+    name  = "backendAzureRmResourceGroupLocation"
+    value = "uksouth"
+  }
+
+  variable {
+    name  = "backendAzureRmResourceGroupName"
+    value = "TerraformServices-rg"
+  }
+
+  variable {
+    name  = "backendAzureRmStorageAccountName"
+    value = "terraformsvcsprodsa"
+  }
+
+  variable {
+    name  = "backendServiceArm"
+    value = "ARM-Alpha"
+  }
+}
+
 resource "azuredevops_variable_group" "terraform-global" {
   project_id   = local.project_id
   name         = "terraform-global"
