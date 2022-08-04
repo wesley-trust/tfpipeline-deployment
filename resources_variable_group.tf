93a8f463828c4e513a8bacb2793af0788dd3d40c
wesley-trust <wesley-trust@users.noreply.github.com>
2022-08-04 17:33:11 +0100

    Update to not specify backend dependent variables

--- resources_variable_group.tf
+++ resources_variable_group.tf
@@ -1,35 +1,3 @@
-resource "azuredevops_variable_group" "terraform-backend" {
-  project_id   = local.project_id
-  name         = "terraform-backend-01"
-  description  = "Managed by Terraform"
-  allow_access = true
-
-  variable {
-    name  = "backendAzureRmKey"
-    value = "terraform.tfstate"
-  }
-
-  variable {
-    name  = "backendAzureRmResourceGroupLocation"
-    value = "uksouth"
-  }
-
-  variable {
-    name  = "backendAzureRmResourceGroupName"
-    value = "TerraformServices-rg"
-  }
-
-  variable {
-    name  = "backendAzureRmStorageAccountName"
-    value = "terraformsvcsprodsa"
-  }
-
-  variable {
-    name  = "backendServiceArm"
-    value = "ARM-Alpha"
-  }
-}
-
 resource "azuredevops_variable_group" "terraform-global" {
   project_id   = local.project_id
   name         = "terraform-global-01"
